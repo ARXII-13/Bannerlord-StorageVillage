4753c89ebc82108f18672451c279f1fc25a4644c
ARX II-13 <ARX II-13@DESKTOP-220FPVB>
2022-05-22 10:27:24 -0700

    Add project files.

---
+++ StorageVillage/Main.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.Core;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+
+namespace StorageVillage
+{
+    public class Main : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad() { 
+            Module.CurrentModule.AddInitialStateOption( 
+                new InitialStateOption(
+                    "TestMainMenuOption", 
+                    new TextObject("Click me!", null), 
+                    9990, 
+                    () => {
+                        InformationManager.DisplayMessage(new InformationMessage("InformationMessage"));
+                    }, 
+                    () =>
+                    {
+                        return (false, null);
+                    }));
+        }
+    }
+}
