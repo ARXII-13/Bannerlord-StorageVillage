bd94804abe2325d991b2400b93e9a10d2641566f
ARXII-13 <FranciswangZQ@outlook.com>
2025-11-29 10:52:17 -0800

    Fix (#17)

--- StorageVillage/src/data/SaveDefiner.cs
+++ StorageVillage/src/data/SaveDefiner.cs
@@ -1,4 +1,6 @@
-﻿using TaleWorlds.SaveSystem;
+﻿using System.Collections.Generic;
+using TaleWorlds.CampaignSystem.Roster;
+using TaleWorlds.SaveSystem;
 
 namespace StorageVillage.src.data {
     class SaveDefiner : SaveableTypeDefiner {
@@ -8,22 +10,10 @@ namespace StorageVillage.src.data {
         }
 
         protected override void DefineClassTypes() {
-            //AddStructDefinition(typeof(ExampleStruct), 1);
-
-            //base.AddClassDefinition(typeof(TestData), 1);
-            //base.AddClassDefinition(typeof(TestData), 1);
-
-            //base.AddClassDefinition(typeof(CustomMapNotification), 1);
-            //base.AddStructDefinition(typeof(ExampleStruct), 2);
-            //base.AddClassDefinition(typeof(ExampleNested), 3);
-            //base.AddStructDefinition(typeof(NestedStruct), 4);
         }
 
         protected override void DefineContainerDefinitions() {
-            //base.ConstructContainerDefinition(typeof(List<CustomMapNotification>));
-            //// Both of these are necessary: order isn't important
-            //base.ConstructContainerDefinition(typeof(List<NestedStruct>));
-            //base.ConstructContainerDefinition(typeof(Dictionary<string, List<NestedStruct>>));
+            base.ConstructContainerDefinition(typeof(Dictionary<string, ItemRoster>));
         }
     }
 }
